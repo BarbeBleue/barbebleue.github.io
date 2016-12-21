4ceeb4b275ad2b5d8e0708615c156e3782f2d5ae
Simon Cote-Duchesne <simoncoteduchesne@gmail.com>
2016-12-21 13:10:02 -0500

    table of content js

---
+++ javascript/toc.js
@@ -0,0 +1,67 @@
+function tocFr(){
+
+var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<h2>Sur cette page :</h2>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("article h2").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC +=
+   "</ul>" +
+  "</nav>";
+
+$(".all-questions").prepend(ToC);
+
+}
+
+function tocEn(){
+
+var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<h2>On this page:</h2>" +
+    "<ul>";
+
+var newLine, el, title, link;
+
+$("article h2").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC +=
+   "</ul>" +
+  "</nav>";
+
+$(".all-questions").prepend(ToC);
+
+}
